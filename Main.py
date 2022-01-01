3e4b3255b22eff24e57b91f4ecd932c850ff7ef0
Femi Bolaji <32209950+IMEF-FEMI@users.noreply.github.com>
2022-01-01 13:26:36 +0100

    network callbacks

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
-from Block import Block
-from BlockchainUtils import BlockchainUtils
 from Node import Node
-from Transaction import Transaction
-from TransactionPool import TransactionPool
-from Wallet import Wallet
-from Blockchain import Blockchain
 import pprint
-from AccountModel import AccountModel
+import sys
 
 if __name__ == '__main__':
-    node = Node()
+
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+
+    node = Node(ip, port)
+    node.startP2P()
     
+    if port == 10002:
+        node.p2p.connect_with_node('localhost', 10001)
